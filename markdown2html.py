eca98e8f4d6009998df85675caeb415a5893d250
ManuRodriguezC <estebanclimb@gmail.com>
2023-08-12 11:27:09 -0500

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -12,13 +12,13 @@ import os
 if __name__ == "__main__":
 
     if len(sys.argv) != 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
 
     if not os.path.isfile(input_file):
-        sys.stderr.write(f"Missing {input_file}")
+        sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
     sys.exit(0)
