0a72edb7828f45b73c0ea9d9cc265e76dc8acc11
ManuRodriguezC <estebanclimb@gmail.com>
2023-08-12 10:45:36 -0500

    Check document

--- markdown2html.py
+++ markdown2html.py
@@ -4,29 +4,33 @@ Convert markdown to html
 This file read readme.md and write in readme.html
 """
 import sys
-import os
 import markdown
 
-# Checks if it receives less than two arguments
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+def markdown_to_html(md_file, output_file):
+    """
+    Check the argv of the sys and the files in the path
+    """
+    try:
+        with open(md_file, "r") as file:
+            markdown_cont = file.read()
+        html_cont = markdown.markdown(markdown_cont)
 
-# Check id the input file exist
-if not os.path.isfile(input_file):
-    sys.stderr.write(f"Missing {input_file}")
-    sys.exit(1)
+        with open(output_file, "w") as file:
+            file.write(html_cont)
 
-# Open the input file and read the information
-with open(input_file, "r") as mdfile:
-    content = mdfile.read()
-    html_content = markdown.markdown(content)
+    except FileNotFoundError:
+        sys.stderr.write(f"Missing {md_file}")
+        sys.exit(1)
 
-# Open the putput file and write information in html
-with open(output_file, "w") as html_file:
-    html_file.write(html_content)
 
-sys.exit(0)
+if __name__ == "__main__":
+
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+
+    md_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    markdown_to_html(md_file, output_file)
