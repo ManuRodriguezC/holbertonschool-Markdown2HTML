79740bcae12199eca9fff095ea4c4cb92b340251
ManuRodriguezC <estebanclimb@gmail.com>
2023-08-12 07:20:15 -0500

    document markdown

--- markdown2html.py
+++ markdown2html.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 import sys
 import os
 import markdown
 
+"""Checks if it receives less than two arguments"""
 if len(sys.argv) < 3:
     sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
     sys.exit(1)
@@ -10,16 +12,18 @@ if len(sys.argv) < 3:
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
+"""Check id the input file exist"""
 if not os.path.isfile(input_file):
     sys.stderr.write(f"Missing {input_file}")
     sys.exit(1)
 
+"""Open the input file and read the information"""
 with open(input_file, "r") as mdfile:
     content = mdfile.read()
     html_content = markdown.markdown(content)
 
+"""Open the putput file and write information in html"""
 with open(output_file, "w") as html_file:
     html_file.write(html_content)
-    
 
 sys.exit(0)
