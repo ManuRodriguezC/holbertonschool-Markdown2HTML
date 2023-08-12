278e6a8d2ab444e527e7ef9019f9f28cd329724e
ManuRodriguezC <estebanclimb@gmail.com>
2023-08-12 11:21:33 -0500

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,36 +1,24 @@
 #!/usr/bin/python3
-"""
-Convert markdown to html
-This file read readme.md and write in readme.html
-"""
+'''
+This module check the files params in the sys
+
+Argv[1] is a file input
+Argv[2] is a file output
+'''
 import sys
-import markdown
-
-
-def markdown_to_html(md_file, output_file):
-    """
-    Check the argv of the sys and the files in the path
-    """
-    try:
-        with open(md_file, "r") as file:
-            markdown_cont = file.read()
-        html_cont = markdown.markdown(markdown_cont)
-
-        with open(output_file, "w") as file:
-            file.write(html_cont)
-
-    except FileNotFoundError:
-        sys.stderr.write(f"Missing {md_file}")
-        sys.exit(1)
+import os
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
 
-    md_file = sys.argv[1]
+    input_file = sys.argv[1]
     output_file = sys.argv[2]
 
-    markdown_to_html(md_file, output_file)
+    if not os.path.isfile(input_file):
+        sys.stderr.write(f"Missing {input_file}")
+        sys.exit(1)
+    sys.exit(0)
